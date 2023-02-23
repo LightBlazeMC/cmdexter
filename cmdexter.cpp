5d79a7d98b737465c21d19dc05b0dc0d3e5775dd
Mac <maciek12292@gmail.com>
2023-02-23 18:11:38 +0000

    new features added

--- cmdexter.cpp
+++ cmdexter.cpp
@@ -1,20 +1,226 @@
-// cmdexter.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// cmdexter main
 
 #include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+void dexExit() {
+    exit;
+}
+
+void waitForKey() {
+    cout << "Press any key to continue...";
+    cin.get();
+    cin.get();
+}
+
+void waitForKey_Silent() {
+    //cout << endl << "Press any key to continue...";
+    cin.get();
+    cin.get();
+}
+
+void dexShell() {
+    cout << endl << endl << "-----[dexShell Console ";
+    string line;
+    ifstream myfile("MAINDATA/ShellVersion.dex");
+    if (myfile.is_open())
+    {
+        while (getline(myfile, line))
+        {
+            cout << line;
+        }
+        myfile.close();
+        cout << "]-----";
+    }
+    else cout << endl << "[Error!] Unable to detect shell version. (File 'ShellVersion.dex' is missing.)";
+
+    cout << endl << ">> ";
+    string shellIn;
+    cin >> shellIn;
+
+    if (shellIn == "dex.exit(0)") {
+        cout << endl << "[i] Shell exited with reason code 0." << endl;
+        waitForKey();
+        cout << endl << endl << "You may now close the program safely.";
+        dexExit();
+        exit;
+        while (1 != 0) {
+            exit;
+        }
+    }
+
+    else if (shellIn == "dex.exit(2)") {
+        cout << endl << "[i] Shell exited with reason code 2." << endl;
+        waitForKey();
+        dexExit();
+    }
+
+    else if (shellIn == "dex.exit(1)") {
+        cout << endl << "[i] Shell restarted with reason code 1." << endl;
+        waitForKey();
+        dexShell();
+    }
+
+    else if (shellIn == "dex.exit()") {
+        cout << endl << "[ERROR!] No exit reason code provided." << endl << "[i] Type 'dex.exit(help)' to learn how to properly terminate your session." << endl;
+        waitForKey();
+        dexShell();
+    }
+
+    else if (shellIn == "dex.exit(help)") {
+        cout << endl << endl;
+        string line;
+        ifstream myfile("MAINDATA/exit_HelpFile.dex");
+        if (myfile.is_open())
+        {
+            while (getline(myfile, line))
+            {
+                cout << line << "\n";
+            }
+            myfile.close();
+        }
+        else
+        {
+            cout << endl << "[Error!] Unable to load help file. (File 'exit_HelpFile.dex' is missing.)";
+        }
+        cout << endl;
+        waitForKey();
+        dexShell();
+    }
+
+    else if (shellIn == "dex.help()") {
+        string line;
+        ifstream myfile("MAINDATA/HelpFile.dex");
+        if (myfile.is_open())
+        {
+            while (getline(myfile, line))
+            {
+                cout << line << "\n";
+            }
+            myfile.close();
+        }
+        else
+        {
+            cout << endl << "[Error!] Unable to load help file. (File 'HelpFile.dex' is missing.)";
+        }
+        cout << endl;
+        waitForKey();
+        dexShell();
+    }
+
+    else if (shellIn == "dex.sdk()") {
+        string line;
+        ifstream myfile("MAINDATA/SDK.dex");
+        if (myfile.is_open())
+        {
+            while (getline(myfile, line))
+            {
+                cout << line << '\n';
+            }
+            myfile.close();
+        }
+        else cout << endl << "[Error!] No SDK install present. (File 'sdk.dex' is missing.)";
+        cout << endl;
+        waitForKey();
+        dexShell();
+    }
+
+    else if (shellIn == "dex.install(start)") {
+        cout << endl << "To install a new package, place the 'install.pkdex' file in the main CMDEXTER directory." << endl;
+        waitForKey();
+        cout << endl;
+
+        char oldname[] = "install.pkdex";
+        char newname[] = "install.exe";
+
+        /*	Deletes the file if exists */
+        if (rename(oldname, newname) != 0) {
+            perror("[Error!] Could not import package");
+            cout << endl;
+            dexShell();
+        }
+        else {
+            cout << endl << "Package imported successfully." << endl;
+        }
+
+        cout << endl;
+        system("install.exe");
+        cout << endl;
+        cout << endl << "Installer has exited." << endl;
+
+        char e_oldname[] = "install.exe";
+        char e_newname[] = "install.pkdex";
+
+        /*	Deletes the file if exists */
+        if (rename(e_oldname, e_newname) != 0) {
+            perror("[Error!] Could not export package");
+            cout << endl;
+            dexShell();
+        }
+        else {
+            cout << endl << "Package exported successfully." << endl;
+        }
+
+        dexShell();
+    }
+
+    else if (shellIn == "dex.install(exe2dex)") {
+    cout << endl << "Windows Executable (EXE) will be converted to a CMDEXTER Package File (PKDEX)" << endl;
+    cout << endl << "Place the 'install.exe' file in the main CMDEXTER directory." << endl;
+    waitForKey();
+    cout << endl;
+
+    char oldname[] = "install.exe";
+    char newname[] = "install.pkdex";
+
+    /*	Deletes the file if exists */
+    if (rename(oldname, newname) != 0) {
+        perror("[Error!] Could not convert package");
+        cout << endl;
+        dexShell();
+    }
+    else {
+        cout << endl << "Package converted successfully." << endl;
+    }
+    dexShell();
+    }
+
+    else
+    {
+        cout << endl << "[Error!] Unrecognised parameter: " << shellIn << endl;
+        dexShell();
+    }
+}
+
+
+void dexSys() {
+    cout << "CMDEXTER" << endl;
+
+    cout << "Current version: ";
+    string line;
+    ifstream myfile("MAINDATA/CurrentVersion.dex");
+    if (myfile.is_open())
+    {
+        while (getline(myfile, line))
+        {
+            cout << line << '\n';
+        }
+        myfile.close();
+    }
+    else cout << endl << "[Error!] Unable to detect software version. (File 'CurrentVersion.dex' is missing.)";
+
+    cout << endl;
+    cout << "Starting shell..." << endl << endl;
+    dexShell();
+}
+
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    dexSys();
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
